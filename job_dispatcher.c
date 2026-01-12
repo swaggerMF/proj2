03e165c446fcf4a44389f773d701f0945f8bdb5a
vladsimion <vlad@simi>
2026-01-12 23:18:02 +0200

    first commit

---
+++ job_dispatcher.c
@@ -0,0 +1,60 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MASTER_RANK 0
+#define CLIENT_ID_LEN 32
+#define NAME_LEN 9
+
+enum {
+    CMD_PRIMES = 1,
+    CMD_PRIMEDIVISORS = 2,
+    CMD_ANAGRAMS = 3
+};
+
+enum {
+    TAG_WORK = 1,
+    TAG_STOP = 2,
+    TAG_RESULT_HEADER = 3,
+    TAG_RESULT_STRING = 4
+};
+
+typedef struct {
+    int job_id;
+    int cmd;
+    long long n;
+    char client[CLIENT_ID_LEN];
+    char name[NAME_LEN];
+} JobPacket;
+
+typedef struct {
+    int job_id;
+    int cmd;
+    long long num_result;
+    int anagram_count;
+    int str_len;
+    char client[CLIENT_ID_LEN];
+} ResultPacket;
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank = -1;
+    int comm_sz = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    if (rank == MASTER_RANK) {
+        if (argc != 2) {
+            fprintf(stderr, "Usage: mpirun -np <P> %s <command_file>\n", argv[0]);
+        } else if (comm_sz == 1) {
+            fprintf(stdout, "serial mode placeholder\n");
+        } else {
+            fprintf(stdout, "parallel mode placeholder\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
